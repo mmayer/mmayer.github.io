6a36a8e5c9a920031f940f657ba0a9f48a38988d
Markus Mayer <code@mmayer.net>
2021-10-22 16:28:50 -0700

    update_copy.sh: look for GNU sed

--- update_copy.sh
+++ update_copy.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 prg=`basename $0`
 
+# We need GNU sed. On a Mac, "sed" is likely not GNU sed, therefore...
+sed=`which gsed` 2>/dev/null
+test "$sed" = "" && sed=`which sed`
+
 year=`date '+%Y'`
-sed -i "s/^&copy; .*/\&copy; $year/" index.md
+$sed -i "s/^&copy; .*/\&copy; $year/" index.md
 
 if git status index.md | grep modified >/dev/null; then
 	echo "Updated copyright to $year..."
