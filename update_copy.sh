924d6139d1906e877000fe076cce556efdb7c55f
Markus Mayer <code@mmayer.net>
2021-10-22 16:35:35 -0700

    update_copy.sh: check first if we even need to update index.md

--- update_copy.sh
+++ update_copy.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 prg=`basename $0`
 
+year=`date '+%Y'`
+if grep "^&copy; $year" index.md >/dev/null; then
+	# Already up to date. Nothing to do.
+	exit 0
+fi
+
 # We need GNU sed. On a Mac, "sed" is likely not GNU sed, therefore...
 sed=`which gsed` 2>/dev/null
 test "$sed" = "" && sed=`which sed`
 
-year=`date '+%Y'`
 $sed -i "s/^&copy; .*/\&copy; $year/" index.md
 
 if git status index.md | grep modified >/dev/null; then
